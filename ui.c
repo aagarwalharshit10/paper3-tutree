e2c904f5054d94f2b6a1c99d98267bf646479128
aagarwalharshit10 <64679122+aagarwalharshit10@users.noreply.github.com>
2021-05-27 13:27:20 +0530

    Create ui.c
    union and intersection of two sorted arrays

---
+++ ui.c
@@ -0,0 +1,58 @@
+// C program to find intersection of
+// two sorted arrays
+#include <stdio.h>
+
+/* Function prints Intersection of arr1[] and arr2[]
+m is the number of elements in arr1[]
+n is the number of elements in arr2[] */
+void printIntersection(int arr1[], int arr2[], int m, int n)
+{
+	int i = 0, j = 0;
+	while (i < m && j < n) {
+		if (arr1[i] < arr2[j])
+			i++;
+		else if (arr2[j] < arr1[i])
+			j++;
+		else /* if arr1[i] == arr2[j] */
+		{
+			printf(" %d ", arr2[j++]);
+			i++;
+		}
+	}
+}
+void printUnion(int arr1[], int arr2[], int m, int n)
+{
+    int i = 0, j = 0;
+    while (i < m && j < n) {
+        if (arr1[i] < arr2[j])
+            printf(" %d ", arr1[i++]);
+        else if (arr2[j] < arr1[i])
+            printf(" %d ", arr2[j++]);
+        else {
+            printf(" %d ", arr2[j++]);
+            i++;
+        }
+    }
+ 
+    /* Print remaining elements of the larger array */
+    while (i < m)
+        printf(" %d ", arr1[i++]);
+    while (j < n)
+        printf(" %d ", arr2[j++]);
+}
+
+/* Driver program to test above function */
+int main()
+{
+	int arr1[] = { 1, 3, 4, 5, 7 };
+	int arr2[] = { 2, 3, 5, 6 };
+	int m = sizeof(arr1) / sizeof(arr1[0]);
+	int n = sizeof(arr2) / sizeof(arr2[0]);
+	printf("Union:");
+	printUnion(arr1, arr2, m, n);
+	printf("\n");
+	printf("Intersection:");
+	printIntersection(arr1, arr2, m, n);
+	getchar();
+	return 0;
+}
